af89f3c0c2199e0cb63eca13a3683824c6b3078c
ruoxingwang-sudo <ruoxing.wang@uqconnect.edu.au>
2023-03-07 23:34:39 +1000

    03072023

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("llllll");
+
+const h1 = document.querySelector("h1")
+
+console.log(h1);
+
+const content = document.querySelector('.content')
+
+console.log(content);
+
+
+const img = document.querySelectorAll('footer a img')
+
+console.log(img[0]);
+console.log(img[1]);
+console.log(img[2]);
