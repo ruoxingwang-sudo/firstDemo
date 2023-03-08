7437464ff6cacff27e49ae8020c0dbc4cfdad407
ruoxingwang-sudo <ruoxing.wang@uqconnect.edu.au>
2023-03-09 00:27:24 +1000

    final

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ const img = document.querySelectorAll('footer a img')
 console.log(img[0]);
 console.log(img[1]);
 console.log(img[2]);
+console.log(img[3]);
+
+const explore = document.querySelector('#explore')
+
+console.log(explore);
+
+explore.addEventListener('click',() => {
+  h1.style.color = '#03a8f4'
+})
